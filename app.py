c02abf542ca4251602ef616abf48373a1bce6077
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-04-13 16:18:00 -0500

    adds route for get and post data

--- app.py
+++ app.py
@@ -1,50 +1,68 @@
-import re
-import requests
-import psycopg2
-from bs4 import BeautifulSoup
+from flask import Flask
 
-# Connect to the database
-conn = psycopg2.connect(database="song_4eid", user="mitch", password="lOhQzEazORh9ISV2WWHNbORJtbYDOX3m", host="dpg-cgqsoiu4dadbdtfd2ir0-a.ohio-postgres.render.com", port="5432")
-cur = conn.cursor()
 
-# Truncate the table
-cur.execute("TRUNCATE TABLE GHM")
+app = Flask(__name__)
 
-# Fetch data from the website
-url = 'https://greenheartmeals.com/currentmenu/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+@app.route('/')
+def run_script():
+    import psycopg2
+    import requests
+    from bs4 import BeautifulSoup
+    import re
 
-# find the menu details
-buttons = soup.find_all('button', class_='new_button_square_small')
-menu_title = soup.find_all('p', {'class': 'menu_title'})
-menu_descr = soup.find_all('p', {'class': 'menu_description'})
-menu_imgs = soup.find_all('div', {'class': 'col-sm-4 col-xs-4 p-0'})
+    # connect to the SQLite database
+    conn = psycopg2.connect(
+        host="dpg-cgqsoiu4dadbdtfd2ir0-a.ohio-postgres.render.com",
+        database="song_4eid",
+        user="mitch",
+        password="lOhQzEazORh9ISV2WWHNbORJtbYDOX3m"
+    )
+    cur = conn.cursor()
 
-menu_items = []
+    # Truncate the table
+    cur.execute("TRUNCATE TABLE GHM")
 
-# Extract data from the website and insert into the database
-# zip combines tuples into tuples. so you dont hav multiple for loop
-for button, title, descr, div in zip(buttons, menu_title, menu_descr, menu_imgs):
-    # finding the images and returning the url in src
-    onclick = button.get('onclick')
-    img_tag = div.find('img', src=True)
-    match = re.search(r'openModalMealsByUnit\((\d+),\s*\'(\d{4}-\d{2}-\d{2})\'\)', onclick)
-    if match:
-        img_url = img_tag['src']
-        id, dd = match.groups()
-        menu_items.append({
-            'title': title.text.strip(),
-            'id': id,
-            'dd': dd,
-            'img_url': img_url,
-            'description': descr.text.strip()
-        })
-        # Insert data into the database
-        cur.execute("INSERT INTO GHM (title, id, dd, img_url, description) VALUES (%s, %s, %s, %s, %s)", (title.text.strip(), id, dd, img_url, descr.text.strip()))
-# Commit the changes to the database
-cur.execute("INSERT INTO GHM (title, id, dd, img_url, description) VALUES ('The Great Gatsby', 123, '2022-05-01', 'https:y.jpg','test.com')")
-conn.commit()
+    # Fetch data from the website
+    url = 'https://greenheartmeals.com/currentmenu/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Close the database connection
-conn.close()
+    # find the menu details
+    buttons = soup.find_all('button', class_='new_button_square_small')
+    menu_title = soup.find_all('p', {'class': 'menu_title'})
+    menu_descr = soup.find_all('p', {'class': 'menu_description'})
+    menu_imgs = soup.find_all('div', {'class': 'col-sm-4 col-xs-4 p-0'})
+
+    menu_items = []
+
+    # Extract data from the website and insert into the database
+    # zip combines tuples into tuples. so you dont hav multiple for loop
+    for button, title, descr, div in zip(buttons, menu_title, menu_descr, menu_imgs):
+        # finding the images and returning the url in src
+        onclick = button.get('onclick')
+        img_tag = div.find('img', src=True)
+        match = re.search(r'openModalMealsByUnit\((\d+),\s*\'(\d{4}-\d{2}-\d{2})\'\)', onclick)
+        if match:
+            img_url = img_tag['src']
+            id, dd = match.groups()
+            menu_items.append({
+                'title': title.text.strip(),
+                'id': id,
+                'dd': dd,
+                'img_url': img_url,
+                'description': descr.text.strip()
+            })
+            # Insert data into the database
+            cur.execute("INSERT INTO GHM (title, id, dd, img_url, description) VALUES (%s, %s, %s, %s, %s)", (title.text.strip(), id, dd, img_url, descr.text.strip()))
+
+    # Commit the changes to the database
+    cur.execute("INSERT INTO GHM (title, id, dd, img_url, description) VALUES ('The Great Gatsby', 123, '2022-05-01', 'https:y.jpg','test.com')")
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
+
+    return 'Data successfully uploaded'
+
+if __name__ == '__main__':
+    app.run()
