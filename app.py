a704452bc6a1b811d717137554d1bce68f905c87
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-04-13 16:49:01 -0500

    adds full code w/ test

--- app.py
+++ app.py
@@ -8,54 +8,54 @@ import re
 app = Flask(__name__)
 CORS(app)
 
-conn = psycopg2.connect(
-    host="dpg-cgqsoiu4dadbdtfd2ir0-a.ohio-postgres.render.com",
-    database="song_4eid",
-    user="mitch",
-    password="lOhQzEazORh9ISV2WWHNbORJtbYDOX3m"
-)
-
 
 # Post request
 @app.route('/update')
 def run_script():
+    # connect to DB
+    conn = psycopg2.connect(
+        host="dpg-cgqsoiu4dadbdtfd2ir0-a.ohio-postgres.render.com",
+        database="song_4eid",
+        user="mitch",
+        password="lOhQzEazORh9ISV2WWHNbORJtbYDOX3m"
+    )
     cur = conn.cursor()
 
     # Truncate the table
     cur.execute("TRUNCATE TABLE GHM")
 
     # Fetch data from the website
-    # url = 'https://greenheartmeals.com/currentmenu/'
-    # response = requests.get(url)
-    # soup = BeautifulSoup(response.content, 'html.parser')
-    #
-    # # find the menu details
-    # buttons = soup.find_all('button', class_='new_button_square_small')
-    # menu_title = soup.find_all('p', {'class': 'menu_title'})
-    # menu_descr = soup.find_all('p', {'class': 'menu_description'})
-    # menu_imgs = soup.find_all('div', {'class': 'col-sm-4 col-xs-4 p-0'})
-    #
-    # menu_items = []
-    #
-    # # Extract data from the website and insert into the database
-    # # zip combines tuples into tuples. so you dont hav multiple for loop
-    # for button, title, descr, div in zip(buttons, menu_title, menu_descr, menu_imgs):
-    #     # finding the images and returning the url in src
-    #     onclick = button.get('onclick')
-    #     img_tag = div.find('img', src=True)
-    #     match = re.search(r'openModalMealsByUnit\((\d+),\s*\'(\d{4}-\d{2}-\d{2})\'\)', onclick)
-    #     if match:
-    #         img_url = img_tag['src']
-    #         id, dd = match.groups()
-    #         menu_items.append({
-    #             'title': title.text.strip(),
-    #             'id': id,
-    #             'dd': dd,
-    #             'img_url': img_url,
-    #             'description': descr.text.strip()
-    #         })
-    #         # Insert data into the database
-    #         cur.execute("INSERT INTO GHM (title, id, dd, img_url, description) VALUES (%s, %s, %s, %s, %s)", (title.text.strip(), id, dd, img_url, descr.text.strip()))
+    url = 'https://greenheartmeals.com/currentmenu/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # find the menu details
+    buttons = soup.find_all('button', class_='new_button_square_small')
+    menu_title = soup.find_all('p', {'class': 'menu_title'})
+    menu_descr = soup.find_all('p', {'class': 'menu_description'})
+    menu_imgs = soup.find_all('div', {'class': 'col-sm-4 col-xs-4 p-0'})
+
+    menu_items = []
+
+    # Extract data from the website and insert into the database
+    # zip combines tuples into tuples. so you dont hav multiple for loop
+    for button, title, descr, div in zip(buttons, menu_title, menu_descr, menu_imgs):
+        # finding the images and returning the url in src
+        onclick = button.get('onclick')
+        img_tag = div.find('img', src=True)
+        match = re.search(r'openModalMealsByUnit\((\d+),\s*\'(\d{4}-\d{2}-\d{2})\'\)', onclick)
+        if match:
+            img_url = img_tag['src']
+            id, dd = match.groups()
+            menu_items.append({
+                'title': title.text.strip(),
+                'id': id,
+                'dd': dd,
+                'img_url': img_url,
+                'description': descr.text.strip()
+            })
+            # Insert data into the database
+            cur.execute("INSERT INTO GHM (title, id, dd, img_url, description) VALUES (%s, %s, %s, %s, %s)", (title.text.strip(), id, dd, img_url, descr.text.strip()))
 
     # Commit the changes to the database
     cur.execute(
@@ -71,6 +71,13 @@ def run_script():
 # Get request
 @app.route('/ghm')
 def get_ghm_menu():
+    # connect to DB
+    conn = psycopg2.connect(
+        host="dpg-cgqsoiu4dadbdtfd2ir0-a.ohio-postgres.render.com",
+        database="song_4eid",
+        user="mitch",
+        password="lOhQzEazORh9ISV2WWHNbORJtbYDOX3m"
+    )
     # Create a cursor object
     cur = conn.cursor()
 
